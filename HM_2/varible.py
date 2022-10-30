1df8af29d92e56a025bbb2f9eae6e1d6c4e8a90f
yurii13 <tymoshchuk888@gmail.com>
2022-10-30 16:55:28 +0200

    add file

--- HM_2/varible.py
+++ HM_2/varible.py
@@ -1,21 +1,30 @@
-third_number = second_number
-second_number = first_number
-first_number = third_number
+# third_number = second_number
+# second_number = first_number
+# first_number = third_number
+#
+# print(first_number, second_number)
+#
+# # заміна значень 2х змінних використовуючи властивості python - рузультат вивести на/в термінал
+# first_number = 13
+# second_number = 19
+# first_number, second_number = second_number, first_number
+#
+# print(first_number, second_number)
+#
+# # (завдання з зірочкою +3 бали до завдання) заміна значень 2 змінних не використівуючи 2х попредніх варіантів.
+# first_number = 13
+# second_number = 19
+# first_number = first_number + second_number
+# second_number = first_number - second_number
+# first_number = first_number - second_number
+#
+# print(first_number, second_number)
 
-print(first_number, second_number)
 
-# заміна значень 2х змінних використовуючи властивості python - рузультат вивести на/в термінал
-first_number = 13
-second_number = 19
-first_number, second_number = second_number, first_number
+a = 0
+while True:
+    print(2 ** a)
+    a += 1
 
-print(first_number, second_number)
-
-# (завдання з зірочкою +3 бали до завдання) заміна значень 2 змінних не використівуючи 2х попредніх варіантів.
-first_number = 13
-second_number = 19
-first_number = first_number + second_number
-second_number = first_number - second_number
-first_number = first_number - second_number
-
-print(first_number, second_number)
+    if a > 10:
+        break
