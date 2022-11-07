032f96e4f51dc585769e74ceca394bcaa118c230
yurii13 <tymoshchuk888@gmail.com>
2022-11-07 23:35:12 +0200

    add file

--- HW_5/task_2.py
+++ HW_5/task_2.py
@@ -2,18 +2,7 @@
 # Наприклад 25 в квадраті еквівалентно 625.
 # Напишіть програму яка запрошує число N і виводить усі антропоморфні числа що не більше за N.
 
-# i = 5
-# i_squared = i ** 2
-# print(i_squared)
-# if str(i_squared).endswith(str(i)):
-#   pass
 
-# number = int(input('Вdедіть число >>> '))
-# for i in range(1, number):
-#     if i * i < number:
-#         print(i*i)
-#         if i in i*i:
-#             print(i*i)
 
 number = int(input('Введіть число >>> '))
 resultList = []
